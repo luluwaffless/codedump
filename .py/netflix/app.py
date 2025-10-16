bbc7447fa182db58414798fe31b98101df07dd7c
luluwaffless <lcsgoes210@gmail.com>
2025-10-15 21:08:37 -0300

    changes

--- .py/netflix/app.py
+++ .py/netflix/app.py
@@ -1,4 +1,4 @@
-from pyautogui import moveTo, click
+from pyautogui import moveTo, click, press
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 import webbrowser
@@ -14,6 +14,21 @@ async def root():
 async def skip():
     moveTo(CLICK)
     click(CLICK)
+    return 200
+
+@app.post("/left")
+async def left():
+    press("left")
+    return 200
+
+@app.post("/right")
+async def right():
+    press("right")
+    return 200
+
+@app.post("/space")
+async def space():
+    press("space")
     return 200
 
 webbrowser.open("http://localhost")
